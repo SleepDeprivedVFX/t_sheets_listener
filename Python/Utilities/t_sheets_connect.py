4e744922a9a3d9bae4d35596083d12253280da0d
Adam Benson <AdamBenson.vfx@gmail.com>
2017-11-02 13:29:04 -0700

    Added some things and updated some things from the working version in Shotgun.

--- Python/Utilities/t_sheets_connect.py
+++ Python/Utilities/t_sheets_connect.py
@@ -53,6 +53,33 @@ class t_sheets_connect:
         self.jobs_list = urllib.urlencode(jobs_params)
         self.jobs_request = urllib2.Request('%sjobcodes?%s' % (url, self.jobs_list), headers=headers)
 
+    def _send_to_tsheets(self, page=None, data=None):
+        if page:
+            if data:
+                packed_data = json.dumps(data)
+                request = urllib2.Request('%s%s' % (url, page), headers=headers, data=packed_data)
+                request.add_header('Content-Type', 'application/json')
+                response = urllib2.urlopen(request)
+                response_data = json.loads(response.read())
+                return response_data
+            else:
+                return False
+        else:
+            return False
+
+    def _return_from_tsheets(self, page=None, data=None):
+        if page:
+            if data:
+                data_list = urllib.urlencode(data)
+                request = urllib2.Request('%s%s?%s' % (url, page, data_list), headers=self.headers)
+                response = urllib2.urlopen(request)
+                response_data = json.loads(response.read())
+                return response_data
+            else:
+                return False
+        else:
+            return False
+
     def return_subs(self, job_id=None):
         # this returns all children of a parent job id.  It does not return sub-children.
         if job_id:
@@ -72,28 +99,26 @@ class t_sheets_connect:
 
     def get_ts_active_users(self):
         ts_users = {}
-        user_list = urllib.urlencode(user_params)
-        user_request = urllib2.Request('%susers?%s' % (url, user_list), headers=headers)
-        user_js = json.loads(urllib2.urlopen(user_request).read())
-        for l_type, result_data in user_js.items():
-            if l_type == 'results':
-                user_data = result_data['users']
-                for user in user_data:
-                    data = user_data[user]
-                    first_name = data['first_name']
-                    last_name = data['last_name']
-                    email = data['email']
-                    last_active = data['last_active']
-                    active = data['active']
-                    username = data['username']
-                    user_id = data['id']
-                    name = first_name, last_name
-                    # print first_name, last_name, email
-                    # print last_active, active, username
-                    # print '-' * 150
-                    ts_users[email] = {'name': name, 'last_active': last_active, 'active': active,
-                                       'username': username, 'email': email, 'id': user_id}
-        return ts_users
+        user_params = {'per_page': '50', 'active': 'yes'}
+        user_js = self._return_from_tsheets(page='users', data=user_params)
+        if user_js:
+            for l_type, result_data in user_js.items():
+                if l_type == 'results':
+                    user_data = result_data['users']
+                    for user in user_data:
+                        data = user_data[user]
+                        first_name = data['first_name']
+                        last_name = data['last_name']
+                        email = data['email']
+                        last_active = data['last_active']
+                        active = data['active']
+                        username = data['username']
+                        user_id = data['id']
+                        name = first_name, last_name
+                        ts_users[email] = {'name': name, 'last_active': last_active, 'active': active,
+                                           'username': username, 'email': email, 'id': user_id}
+            return ts_users
+        return False
 
     def get_ts_current_user_status(self, email=None):
         data = {}
@@ -118,9 +143,6 @@ class t_sheets_connect:
                         project_name = data['name']
                         project_id = data['id']
                         ts_projects[project_id] = project_name
-                        # print project_id
-                        # print project_name
-                        # print '=' * 150
         return ts_projects
 
     def get_ts_subs(self, project_id=None):
@@ -334,9 +356,7 @@ class t_sheets_connect:
 
             if not ts_project_id:
                 # If no project is matched, the ID should still be empty.  Create new project in T-Sheets
-                # add_project = self.add_new_ts_project(name=name)
-                # temp 1 test
-                add_project = 1
+                add_project = self.add_new_ts_project(name=name)
                 ts_project_id = add_project
 
             # Check sg assets against ts assets
@@ -344,6 +364,10 @@ class t_sheets_connect:
             sg_project_seq = self.get_shotgun_sequence(project=sg_id)
             # After get_shotgun_sequence() call each shot from within the sequence loop.
             ts_project_jobcodes = self.get_ts_subs(project_id=ts_project_id)
+
+            # ==========================================================================
+            # Package TS Jobcodes into dictionaries for processing
+            # ==========================================================================
             if ts_project_jobcodes:
                 # Confirm that ts_project_jobcodes
                 # Prep storage dictionaries
@@ -367,50 +391,68 @@ class t_sheets_connect:
                             # asset_data = Dictionary of values from the specific jobcode.
                             ts_assets[asset_id] = asset_data['name']
 
-                            # has_tasks = asset_data['tasks']
-                            # if has_tasks:
-                            #     print 'ASSET HAS TASKS...'
-                            #     print asset_data['tasks']
-                            # NOTE: The Task Data stuff is probably irrelevant since T-Sheets has it's own task thing.
                     elif data_type == 'Shots':
                         # First up is sequences
                         for seq_id, seq_data in sub_data.items():
+                            # Each sequence gets saved into the ts_seq dictionary
                             ts_seq[seq_id] = seq_data['name']
+
+                            # Cycle through sequence data to collect and package shots associated with each sequence
                             for shot_id, shot_data in seq_data['shots'].items():
-                                print 'SHOT: %s' % shot_data['name']
+                                # seq_data['shots'] is a dictionary that contains shots for a given sequence
+                                # package that data into the ts_shots dictionary
                                 ts_shots[shot_id] = {'seq_id': seq_id, 'seq_name': seq_data['name'],
                                                      'shot': shot_data['name']}
 
+                # ***************************************************************************************
+                # Compare SG data to TS data and perform additions if items are missing.
+                # ***************************************************************************************
                 if sg_project_assets:
+                    # if there are Assets listed in Shotgun
                     for sg_asset_id, sg_asset in sg_project_assets.items():
+                        # set the sg_asset_found to False until a match is found.
                         sg_asset_found = False
                         for ts_asset in ts_assets.values():
+                            # Cycling through the assets in the ts_asset dictionary. If a match is found, set to True
                             if ts_asset == sg_asset:
-                                print '\t\tSHOTGUN ASSET %s FOUND IN T-Sheets!' % sg_asset
                                 sg_asset_found = True
                                 break
+                        # Check if the asset is found
                         if not sg_asset_found:
+                            # If the asset isn't found in T-Sheets, add it to the database
                             print 'FAIL!!! SHOTGUN ASSET NOT FOUND: %s' % sg_asset
                             print 'Sending to the function that will create the Asset in T-Sheets'
 
                 if sg_project_seq:
-                    for sg_seq_id, sg_seq in sg_project_seq.items():
-                        sg_seq_found = False
-                        for tsid, tsseq in ts_seq.items():
-                            if tsseq == sg_seq:
-                                print 'SHOTGUN SEQUENCE %s FOUND IN T-SHEETS!' % sg_seq
-                                sg_seq_found = True
-                                sg_shots = self.get_shotgun_shots(project=sg_id, sequence=sg_seq_id)
-                                if sg_shots:
-                                    for sg_shot_name in sg_shots.values():
-                                        for tsshot, tsdata in ts_shots.items():
-                                            # print 'test: %s, %s, %s' % (sg_seq, sg_shot_name, tsdata['shot'])
-                                            if sg_seq == tsdata['seq_name'] and sg_shot_name['shot'] == tsdata['shot']:
-                                                print 'SHOTGUN SHOT %s FOUND IN T-Sheets!' % sg_shot_name
-                                                break
-                                break
-                        if not sg_seq_found:
-                            print 'FAIL!!! SHOTUGN SEQ NOT FOUND %s' % sg_seq
+                    # If there are sequences listed in Shotgun
+                    if __name__ == '__main__':
+                        for sg_seq_id, sg_seq in sg_project_seq.items():
+                            # Set the sg_seq_found to False until a match is found
+                            sg_seq_found = False
+                            # Cycle through ts_seq to look for a match
+                            for tsid, tsseq in ts_seq.items():
+                                if tsseq == sg_seq:
+                                    # If the sequence is found, search through for shots and set to True
+                                    sg_seq_found = True
+                                    sg_shots = self.get_shotgun_shots(project=sg_id, sequence=sg_seq_id)
+                                    if sg_shots:
+                                        for sg_shot_name in sg_shots.values():
+                                            for tsshot, tsdata in ts_shots.items():
+                                                # print 'test: %s, %s, %s' % (sg_seq, sg_shot_name, tsdata['shot'])
+                                                if sg_seq == tsdata['seq_name'] and sg_shot_name['shot'] == tsdata['shot']:
+                                                    print 'SHOTGUN SHOT %s FOUND IN T-Sheets!' % sg_shot_name
+                                                    # break if the shot is found
+                                                    break
+                                    # break if the sequence is found
+                                    break
+                            if not sg_seq_found:
+                                # If the sequence isn't found in T-Sheets, then add it to the database
+                                # and then add any shots that may also exist in Shotgun
+                                print 'Add shit to the database.'
+
+                # ***************************************************************************************
+                # END Shotgun to T-Sheets data comparison
+                # ***************************************************************************************
 
     def get_ts_user_timesheet(self, email=None):
         timesheet = {}
@@ -438,6 +480,20 @@ class t_sheets_connect:
                     # For the moment, this may be how the test is achieve to trigger a different clock in response.
         return timesheet
 
+    def add_new_ts_sequence(self, name=None, parent_id=None):
+        print 'Add a new sequence from Shotgun. %s' % name
+        # This may be done with the add_sub_folders() method
+        data = {
+            "data":
+                [
+                    {
+                        "name": "%s" % name,
+                        "billable": "yes",
+                        "assigned_to_all": "yes"
+                    }
+                ]
+        }
+
     def add_new_ts_project(self, name=None):
         print 'Add a new job from Shotgun. %s' % name
         data = {
@@ -458,9 +514,12 @@ class t_sheets_connect:
         check = response_data['results']['jobcodes']['1']['_status_message']
         new_id = response_data['results']['jobcodes']['1']['id']
         if check == 'Created':
+            self.add_sub_folders(proj_id=new_id, sub_folder_name='Design')
+            self.add_sub_folders(proj_id=new_id, sub_folder_name='Assets')
+            self.add_sub_folders(proj_id=new_id, sub_folder_name='Shots')
+            self.add_sub_folders(proj_id=new_id, sub_folder_name='Production Admin')
             return new_id
-        else:
-            return False
+        return False
 
     def get_ts_authorization(self):
         filters = [
@@ -471,6 +530,22 @@ class t_sheets_connect:
         ]
         auth_search = sg.find('CustomNonProjectEntity06', filters, fields)
         print auth_search
+
+    def add_sub_folders(self, proj_id=None, sub_folder_name=None):
+        print 'Add Sub Folders'
+        data = {
+            "data":
+                [
+                    {
+                        "name": "%s" % sub_folder_name,
+                        "billable": "yes",
+                        "assigned_to_all": "yes",
+                        "parent_id": "%s" % proj_id
+                    }
+                ]
+        }
+        response_data = self._send_to_tsheets(page='jobcodes', data=data)
+        return response_data
 
 
 # ----------------------------------------------------------------------------------------------------------------------
