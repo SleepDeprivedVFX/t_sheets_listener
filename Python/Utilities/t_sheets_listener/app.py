ecd9f93438aa30f701a82e1123e08a303756134d
Adam Benson <AdamBenson.vfx@gmail.com>
2017-11-16 01:28:35 -0800

    Working timer features with triggers

--- Python/Utilities/t_sheets_listener/app.py
+++ Python/Utilities/t_sheets_listener/app.py
@@ -17,17 +17,11 @@ class ts_timer:
         r = 0.0
         break_timer = False
         while self.run_time:
+            now_hour = datetime.now().hour
+            now_min = datetime.now().minute
             r += 1.0
             t = float(r/1000)
             if t.is_integer():
-                now_date = str(datetime.date(datetime.now()))
-                now_time = str(datetime.time(datetime.now()))
-                split_date = now_date.split('-')
-                split_time = now_time.split(':')
-                clock_h = int(split_time[0])
-                clock_m = int(split_time[1])
-                get_s = split_time[2].split('.')[0]
-                clock_s = int(get_s)
                 if s < 59:
                     s += 1
                 else:
@@ -39,18 +33,21 @@ class ts_timer:
                         m = 0
                         s = 0
                 print '%s:%s:%s' % (h, m, s)
-                if m >= 1 and not break_timer:
-                    # this will eventually be set to 15 minutes
-                    start_break = datetime.now() - timedelta(minutes=1)
-                    print 'Start Break At: %s' % start_break
-                    break_timer = True
+                if s >= 15 and not break_timer:
+                    if now_hour >= 1 and now_min >= 15:
+                        print 'Min Time condition met'
+                        if now_hour < 14 and now_min < 35:
+                            print 'Max time condition met'
+                            # this will eventually be set to 15 minutes
+                            start_break = datetime.now()  # - timedelta(minutes=1)
+                            print 'Start Break At: %s' % start_break
+                            break_timer = True
 
             if ctypes.windll.user32.GetKeyState(0x01) not in [0, 1]:
                 if break_timer:
                     end_break = datetime.now()
-                    start_s = start_break.strftime('%Y-%m-%d %H:%M:%S')
-                    end_s = end_break.strftime('%Y-%m-%d %H:%M:%S')
-                    break_time = datetime.strptime(end_s, '%Y-%m-%d %H:%M:%S') - datetime.strptime(start_s, '%Y-%m-%d %H:%M:%S')
+                    break_time = end_break - start_break
+                    print 'Open Lunch Menu'
                     break_timer = False
                     print 'Break Time: %s' % break_time
                 s = 0
