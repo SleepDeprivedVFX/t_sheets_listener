744891c82bb721888dc58d479bdfc57a47257ee0
Adam Benson <AdamBenson.vfx@gmail.com>
2017-11-15 21:28:10 -0800

    Found some interesting stuff with the pynput node.  I love the way it works with behaving like a listener and watching click, but I can't get it to do anything timer like with it.

--- Python/Utilities/t_sheets_listener/app.py
+++ Python/Utilities/t_sheets_listener/app.py
@@ -1,28 +1,36 @@
 from libs.pynput import mouse
+import time
 
 
-def on_move(x, y):
-    print('Pointer moved to {0}'.format(
-        (x, y)))
+class t_sheets_listener:
+    def __init__(self):
+        self.h = 0
+        self.m = 0
+        self.s = 0
+        self.ts_timer = True
+        self.run_loop()
+
+    def run_loop(self):
+        while self.ts_timer:
+            self.s += 1
+            print self.s
+            time.sleep(1)
+            if self.s > 20:
+                self.ts_timer = False
 
 
-def on_click(x, y, button, pressed):
-    print('{0} at {1}'.format(
-        'Pressed' if pressed else 'Released',
-        (x, y)))
-    if not pressed:
-        # Stop listener
-        return False
-
-
-def on_scroll(x, y, dx, dy):
-    print('Scrolled {0} at {1}'.format(
-        'down' if dy < 0 else 'up',
-        (x, y)))
+# def on_click(x, y, button, pressed):
+#     # print('{0} at {1}'.format('Pressed' if pressed else 'Released', (x, y)))
+#     s = 0
+#     m = 0
+#     h = 0
+#     if pressed:
+#         print 'Pressed'
 
 # Collect events until released
-with mouse.Listener(
-        on_move=on_move,
-        on_click=on_click,
-        on_scroll=on_scroll) as listener:
-    listener.join()
+# with mouse.Listener(on_click=on_click) as listener:
+#     # d = t_sheets_listener()
+#     listener.join()
+
+mouse.Listener(on_click=on_click)
+
