3331decd6402c68eea8555788159f9ea3060f9a7
Adam Benson <AdamBenson.vfx@gmail.com>
2017-12-05 22:59:44 -0800

    Progress update from Test2. Going to try setting up race condition protections

--- Python/Shotgun/t_sheets_listener/test2.py
+++ Python/Shotgun/t_sheets_listener/test2.py
@@ -5,6 +5,7 @@ import time
 from datetime import datetime
 
 from ui import alert_dialog as ad
+from ui import lunch_break_dialog as lbd
 
 import sys, time
 from PySide.QtGui import *
@@ -33,21 +34,75 @@ class ts_timer(QThread):
         self.signal = ts_signal()
 
     def run(self):
+        s = 0
+        m = 0
+        h = 0
+        r = 0.0
+        break_timer = False
+        start_break = None
+        end_break = None
+        ot_timer = False
+        eod_timer = False
+        while not self.exiting:
+            now_hour = datetime.now().hour
+            now_min = datetime.now().minute
+            r += 1.0
+            t = float(r/1000)
+            if t.is_integer():
+                if s < 59:
+                    s += 1
+                else:
+                    if m < 59:
+                        s = 0
+                        m += 1
+                    elif m == 59 and h <= 22:
+                        h += 1
+                        m = 0
+                        s = 0
+                print '%s:%s:%s' % (h, m, s)
+                now_time = datetime.strptime('%s:%s' % (now_hour, now_min), '%H:%M')
+                lunch_start = datetime.strptime(lunch_start_time, '%H:%M')
+                lunch_end = datetime.strptime(lunch_end_time, '%H:%M')
+                if s >= 5 and not break_timer:
+                    if lunch_start <= now_time < lunch_end:
+                        # this will eventually be set to 15 minutes
+                        start_break = datetime.now()  # - timedelta(minutes=1)
+                        break_timer = True
+                        print 'Start Break At: %s' % start_break
+
+            if ctypes.windll.user32.GetKeyState(0x01) not in [0, 1]:
+                if break_timer:
+                    end_break = datetime.now()
+                    break_time = end_break - start_break
+                    print 'Open Lunch Menu'
+                    break_timer = False
+                    print 'Break Time: %s' % break_time
+                    # self.alert_message = ts_alert()
+                    # self.alert_message.show()
+                    self.signal.lunch.emit('"startBreak": %s, "endBreak": %s, "breakTotal": %s' % (start_break,
+                                                                                                   end_break,
+                                                                                                   break_time))
+
+                s = 0
+                m = 0
+                h = 0
+            time.sleep(0.001)
+
         # The Following is where I would put my timer math and triggers and shit... I think.
-        end = time.time()+10
-        while self.exiting==False:
-            sys.stdout.write('*')
-            sys.stdout.flush()
-            time.sleep(1)
-            now = time.time()
-            if now>=end:
-                self.exiting=True
+        # end = time.time()+10
+        # while self.exiting==False:
+        #     sys.stdout.write('*')
+        #     sys.stdout.flush()
+        #     time.sleep(1)
+        #     now = time.time()
+        #     if now>=end:
+        #         self.exiting=True
 
         # If what I figured out is correct in the ts_signal(), then in order to trigger one dialog or another,
         # I would need different Signal(str) setup and then triggered from the timer loop.
-        self.signal.sig.emit('OK')
-        self.signal.alert.emit('Can this be anything?')
-        self.signal.lunch.emit('Maybe: this, Can: too')
+
+        # self.signal.sig.emit('OK')
+        # self.signal.alert.emit('Can this be anything?')
 
 
 
@@ -97,7 +152,7 @@ class ts_main(QMainWindow):
         # def longoperationcomplete, which resides in this class.  This dynamically updates the main window.
         # I will want to do similar things with the dialog boxes.
         # The following line connects ANY signal from the ts_timer to the longoperationcomplete
-        self.run_ts_timer.signal.sig.connect(self.longoperationcomplete)
+        self.run_ts_timer.signal.lunch.connect(self.open_lunch_break)
         # I think I may have figured this out in the ts_signal() class.  See above.
         # It also means that longoperationcomlete() should be one of the dialog boxes.
         # The next catch is... how do I run a Dialog box, and how do I pass data to it.  Perhaps, multiple signals?
@@ -148,9 +203,19 @@ class ts_main(QMainWindow):
             self.label2.setText('Long operation started')
             self.longbutton.setEnabled(False)
 
-    def longoperationcomplete(self,data):
-        self.label2.setText('Long operation completed with: '+data)
-        self.longbutton.setEnabled(True)
+    def open_lunch_break(self, data=None):
+        print data
+        self.lunch_dialog = QDialog(self)
+        self.lunch_ui = lbd.Ui_Dialog()
+        self.lunch_ui.setupUi(self.lunch_dialog)
+        self.lunch_ui.yes_btn.clicked.connect(self.save_lunch_break)
+        self.lunch_dialog.exec_()
+
+    def save_lunch_break(self):
+        print 'Save lunch break'
+        test_signal = self.lunch_dialog.finished
+        if test_signal:
+            self.lunch_dialog.close()
 
 
 if __name__=='__main__':
