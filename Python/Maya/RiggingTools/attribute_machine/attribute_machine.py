cb22edcf39102784f1e78b35e08a5d4e95bfbed6
Adam Benson <AdamBenson.vfx@gmail.com>
2018-09-21 00:54:37 -0700

    Difficulties getting the radio button attributes to work.  Wrong section for that anyways.

--- Python/Maya/RiggingTools/attribute_machine/attribute_machine.py
+++ Python/Maya/RiggingTools/attribute_machine/attribute_machine.py
@@ -16,9 +16,50 @@ class attr_mach_ui(QtGui.QWidget):
         self.ui.setupUi(self)
         self.ui.do_it.clicked.connect(self.run_it)
         self.ui.dont_do_it.clicked.connect(self.close)
+        self.set_mode()
+
+    def set_mode(self):
+        add = self.ui.add_mode.isChecked()
+        set = self.ui.set_mode.isChecked()
+        connect = self.ui.connect_mode.isChecked()
+        command = self.ui.command_mode.isChecked()
+        if add:
+            selected = cmds.ls(sl=True)
+            attrs = self.collect_attributes(selected=selected)
+            index = 0
+            print attrs
+            for attr in attrs:
+                # self.ui.in_attrs_list.addItem(QtGui.QListWidgetItem(QtGui.QRadioButton('%s' % attr)))
+                # radio_btn = QtGui.QRadioButton('%s' % attr)
+                # radio_btn.setObjectName(attr)
+                # print radio_btn.text()
+                # row = QtGui.QListWidgetItem(radio_btn)
+                # self.ui.in_attrs_list.addItem(row)
+                # index += 1
+                row = QtGui.QTreeWidgetItem()
+                row.setText(0, attr)
+                add_btn = QtGui.QRadioButton()
+                add_btn.setText(attr)
+                add_btn.parent()
+
+        elif set:
+            pass
+        elif connect:
+            pass
+        elif command:
+            pass
 
     def run_it(self):
         pass
+
+    def collect_attributes(self, selected=None):
+        collected_attributes = []
+        for sel in selected:
+            get_attr = cmds.listAttr(sel)
+            for attr in get_attr:
+                if attr not in collected_attributes:
+                    collected_attributes.append(attr)
+        return collected_attributes
 
 if __name__ == '__main__':
     try:
