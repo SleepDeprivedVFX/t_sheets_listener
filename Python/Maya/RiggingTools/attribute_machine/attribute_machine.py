6cc13656f28afd2e8f4f1b83cfd7ebbf5df737eb
Adam Benson <AdamBenson.vfx@gmail.com>
2018-09-22 15:29:12 -0700

    Making progress towards a functional tool

--- Python/Maya/RiggingTools/attribute_machine/attribute_machine.py
+++ Python/Maya/RiggingTools/attribute_machine/attribute_machine.py
@@ -2,6 +2,7 @@ from PySide import QtCore, QtGui
 from maya import cmds
 import sys
 import os
+sys.path.append(r'C:\Users\sleep\OneDrive\Documents\Scripts\Python\Maya\RiggingTools\attribute_machine')
 from ui import attribute_machine_ui as amu
 reload(amu)
 
@@ -16,34 +17,140 @@ class attr_mach_ui(QtGui.QWidget):
         self.ui.setupUi(self)
         self.ui.do_it.clicked.connect(self.run_it)
         self.ui.dont_do_it.clicked.connect(self.close)
+        self.ui.add_mode.clicked.connect(self.set_mode)
+        self.ui.set_mode.clicked.connect(self.set_mode)
+        self.ui.connect_mode.clicked.connect(self.set_mode)
+        self.ui.command_mode.clicked.connect(self.set_mode)
+        self.ui.vector_btn.clicked.connect(self.set_sub_modes)
+        self.ui.float_btn.clicked.connect(self.set_sub_modes)
+        self.ui.int_btn.clicked.connect(self.set_sub_modes)
+        self.ui.bool_btn.clicked.connect(self.set_sub_modes)
+        self.ui.enum_btn.clicked.connect(self.set_sub_modes)
+        self.ui.string_btn.clicked.connect(self.set_sub_modes)
+        self.disable_all()
         self.set_mode()
+
+    def disable_all(self):
+        self.ui.in_attrs.clear()
+        self.ui.out_attrs.clear()
+        self.ui.out_attrs.setEnabled(False)
+        self.ui.in_attrs.setEnabled(False)
+        self.ui.default_val.setEnabled(False)
+        self.ui.min_val.setEnabled(False)
+        self.ui.max_val.setEnabled(False)
+        self.ui.bool_btn.setEnabled(False)
+        self.ui.int_btn.setEnabled(False)
+        self.ui.float_btn.setEnabled(False)
+        self.ui.enum_btn.setEnabled(False)
+        self.ui.enum_list.setEnabled(False)
+        self.ui.string_btn.setEnabled(False)
+        self.ui.vector_btn.setEnabled(False)
+        self.ui.default_val.setEnabled(False)
+        self.ui.min_val_label.setEnabled(False)
+        self.ui.max_val_label.setEnabled(False)
+        self.ui.default_val_label.setEnabled(False)
+        self.ui.values.setEnabled(False)
+        self.ui.value_label.setEnabled(False)
+        self.ui.in_attr_label.setEnabled(False)
+        self.ui.out_attr_label.setEnabled(False)
+
+
+    def set_sub_modes(self):
+        vector_btn = self.ui.vector_btn.isChecked()
+        float_btn = self.ui.float_btn.isChecked()
+        int_btn = self.ui.int_btn.isChecked()
+        string_btn = self.ui.string_btn.isChecked()
+        bool_btn = self.ui.bool_btn.isChecked()
+        enum_btn = self.ui.enum_btn.isChecked()
+        if float_btn:
+            self.ui.enum_list.setEnabled(False)
+            self.ui.enum_label.setEnabled(False)
+            self.ui.min_val.setEnabled(True)
+            self.ui.max_val.setEnabled(True)
+            self.ui.default_val.setEnabled(True)
+            self.ui.min_val_label.setEnabled(True)
+            self.ui.max_val_label.setEnabled(True)
+            self.ui.default_val_label.setEnabled(True)
+            self.ui.value_label.setEnabled(True)
+            self.ui.values.setEnabled(True)
+        elif int_btn:
+            self.ui.enum_list.setEnabled(False)
+            self.ui.enum_label.setEnabled(False)
+            self.ui.min_val.setEnabled(True)
+            self.ui.max_val.setEnabled(True)
+            self.ui.default_val.setEnabled(True)
+            self.ui.min_val_label.setEnabled(True)
+            self.ui.max_val_label.setEnabled(True)
+            self.ui.default_val_label.setEnabled(True)
+            self.ui.value_label.setEnabled(True)
+            self.ui.values.setEnabled(True)
+        elif enum_btn:
+            self.ui.enum_list.setEnabled(True)
+            self.ui.enum_label.setEnabled(True)
+            self.ui.min_val.setEnabled(False)
+            self.ui.max_val.setEnabled(False)
+            self.ui.default_val.setEnabled(False)
+            self.ui.min_val_label.setEnabled(False)
+            self.ui.max_val_label.setEnabled(False)
+            self.ui.default_val_label.setEnabled(False)
+            self.ui.value_label.setEnabled(True)
+            self.ui.values.setEnabled(True)
+        else:
+            self.ui.enum_list.setEnabled(False)
+            self.ui.enum_label.setEnabled(False)
+            self.ui.min_val.setEnabled(False)
+            self.ui.max_val.setEnabled(False)
+            self.ui.default_val.setEnabled(False)
+            self.ui.min_val_label.setEnabled(False)
+            self.ui.max_val_label.setEnabled(False)
+            self.ui.default_val_label.setEnabled(False)
+            self.ui.value_label.setEnabled(True)
+            self.ui.values.setEnabled(True)
+
 
     def set_mode(self):
         add = self.ui.add_mode.isChecked()
+        
         set = self.ui.set_mode.isChecked()
         connect = self.ui.connect_mode.isChecked()
         command = self.ui.command_mode.isChecked()
         if add:
+            self.disable_all()
             selected = cmds.ls(sl=True)
-            attrs = self.collect_attributes(selected=selected)
-            index = 0
-            print attrs
-            for attr in attrs:
-                # self.ui.in_attrs_list.addItem(QtGui.QListWidgetItem(QtGui.QRadioButton('%s' % attr)))
-                # radio_btn = QtGui.QRadioButton('%s' % attr)
-                # radio_btn.setObjectName(attr)
-                # print radio_btn.text()
-                # row = QtGui.QListWidgetItem(radio_btn)
-                # self.ui.in_attrs_list.addItem(row)
-                # index += 1
-                row = QtGui.QTreeWidgetItem()
-                row.setText(0, attr)
-                add_btn = QtGui.QRadioButton()
-                add_btn.setText(attr)
-                add_btn.parent()
+            value = self.ui.values.toPlainText()
+            self.ui.vector_btn.setEnabled(True)
+            self.ui.float_btn.setEnabled(True)
+            self.ui.float_btn.setChecked(True)
+            self.ui.string_btn.setEnabled(True)
+            self.ui.int_btn.setEnabled(True)
+            self.ui.enum_btn.setEnabled(True)
+            self.ui.bool_btn.setEnabled(True)
+            self.set_sub_modes()
+
+            if value:
+                # Oh, shit!  Right!  The Add Attributes shit needs all sorts of variables set for it.
+                # For instance.  Why kind of attribute?  Boolean? String? Enum?
+                # Plus it will need a way to input all of those.
+                pass
 
         elif set:
-            pass
+            self.disable_all()
+            self.ui.values.setEnabled(True)
+            self.ui.value_label.setEnabled(True)
+            self.ui.in_attr_label.setEnabled(True)
+            self.ui.in_attrs.setEnabled(True)
+            selected = cmds.ls(sl=True)
+            attrs = self.collect_attributes(selected=selected)
+            value = self.ui.values.toPlainText()
+            self.ui.vector_btn.setEnabled(True)
+            self.ui.float_btn.setEnabled(True)
+            self.ui.float_btn.setChecked(True)
+            self.ui.string_btn.setEnabled(True)
+            self.ui.int_btn.setEnabled(True)
+            self.ui.enum_btn.setEnabled(True)
+            self.ui.bool_btn.setEnabled(True)
+            self.set_sub_modes()
+
         elif connect:
             pass
         elif command:
--- Python/Maya/RiggingTools/attribute_machine/ui/attribute_machine_ui.py
+++ Python/Maya/RiggingTools/attribute_machine/ui/attribute_machine_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'C:\Users\sleep\OneDrive\Documents\Scripts\Python\Maya\RiggingTools\attribute_machine\ui\attribute_machine_ui.ui'
 #
-# Created: Thu Sep 20 22:18:41 2018
+# Created: Sat Sep 22 14:53:05 2018
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,15 +12,15 @@ from PySide import QtCore, QtGui
 class Ui_machine(object):
     def setupUi(self, machine):
         machine.setObjectName("machine")
-        machine.resize(853, 587)
+        machine.resize(731, 727)
         machine.setStyleSheet("background-color: rgb(100, 100, 100);\n"
 "color: rgb(230, 230, 230);")
-        self.verticalLayout_3 = QtGui.QVBoxLayout(machine)
-        self.verticalLayout_3.setObjectName("verticalLayout_3")
-        self.label = QtGui.QLabel(machine)
-        self.label.setStyleSheet("font: 75 22pt \"BankGothic\";")
-        self.label.setObjectName("label")
-        self.verticalLayout_3.addWidget(self.label)
+        self.verticalLayout_6 = QtGui.QVBoxLayout(machine)
+        self.verticalLayout_6.setObjectName("verticalLayout_6")
+        self.Title = QtGui.QLabel(machine)
+        self.Title.setStyleSheet("font: 75 22pt \"BankGothic\";")
+        self.Title.setObjectName("Title")
+        self.verticalLayout_6.addWidget(self.Title)
         self.modes_group = QtGui.QGroupBox(machine)
         self.modes_group.setMinimumSize(QtCore.QSize(0, 75))
         self.modes_group.setObjectName("modes_group")
@@ -43,66 +43,256 @@ class Ui_machine(object):
         self.command_mode = QtGui.QRadioButton(self.layoutWidget)
         self.command_mode.setObjectName("command_mode")
         self.horizontalLayout.addWidget(self.command_mode)
-        self.verticalLayout_3.addWidget(self.modes_group)
+        self.verticalLayout_6.addWidget(self.modes_group)
+        self.groupBox = QtGui.QGroupBox(machine)
+        self.groupBox.setMinimumSize(QtCore.QSize(0, 100))
+        self.groupBox.setObjectName("groupBox")
+        self.widget = QtGui.QWidget(self.groupBox)
+        self.widget.setGeometry(QtCore.QRect(30, 20, 641, 65))
+        self.widget.setObjectName("widget")
+        self.verticalLayout_5 = QtGui.QVBoxLayout(self.widget)
+        self.verticalLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_5.setObjectName("verticalLayout_5")
+        self.horizontalLayout_7 = QtGui.QHBoxLayout()
+        self.horizontalLayout_7.setObjectName("horizontalLayout_7")
+        self.vector_btn = QtGui.QRadioButton(self.widget)
+        self.vector_btn.setStyleSheet("QRadioButton:disabled {\n"
+"    color: rgb(117, 117, 177);\n"
+"}\n"
+"QRadioButton:enabled {\n"
+"    color: rgb(255, 255, 255);\n"
+"}")
+        self.vector_btn.setObjectName("vector_btn")
+        self.horizontalLayout_7.addWidget(self.vector_btn)
+        self.int_btn = QtGui.QRadioButton(self.widget)
+        self.int_btn.setStyleSheet("QRadioButton:disabled {\n"
+"    color: rgb(117, 117, 177);\n"
+"}\n"
+"QRadioButton:enabled {\n"
+"    color: rgb(255, 255, 255);\n"
+"}")
+        self.int_btn.setObjectName("int_btn")
+        self.horizontalLayout_7.addWidget(self.int_btn)
+        self.string_btn = QtGui.QRadioButton(self.widget)
+        self.string_btn.setStyleSheet("QRadioButton:disabled {\n"
+"    color: rgb(117, 117, 177);\n"
+"}\n"
+"QRadioButton:enabled {\n"
+"    color: rgb(255, 255, 255);\n"
+"}")
+        self.string_btn.setObjectName("string_btn")
+        self.horizontalLayout_7.addWidget(self.string_btn)
+        self.verticalLayout_5.addLayout(self.horizontalLayout_7)
+        self.horizontalLayout_6 = QtGui.QHBoxLayout()
+        self.horizontalLayout_6.setObjectName("horizontalLayout_6")
+        self.float_btn = QtGui.QRadioButton(self.widget)
+        self.float_btn.setStyleSheet("QRadioButton:disabled {\n"
+"    color: rgb(117, 117, 177);\n"
+"}\n"
+"QRadioButton:enabled {\n"
+"    color: rgb(255, 255, 255);\n"
+"}")
+        self.float_btn.setObjectName("float_btn")
+        self.horizontalLayout_6.addWidget(self.float_btn)
+        self.bool_btn = QtGui.QRadioButton(self.widget)
+        self.bool_btn.setStyleSheet("QRadioButton:disabled {\n"
+"    color: rgb(117, 117, 177);\n"
+"}\n"
+"QRadioButton:enabled {\n"
+"    color: rgb(255, 255, 255);\n"
+"}")
+        self.bool_btn.setObjectName("bool_btn")
+        self.horizontalLayout_6.addWidget(self.bool_btn)
+        self.enum_btn = QtGui.QRadioButton(self.widget)
+        self.enum_btn.setStyleSheet("QRadioButton:disabled {\n"
+"    color: rgb(117, 117, 177);\n"
+"}\n"
+"QRadioButton:enabled {\n"
+"    color: rgb(255, 255, 255);\n"
+"}")
+        self.enum_btn.setObjectName("enum_btn")
+        self.horizontalLayout_6.addWidget(self.enum_btn)
+        self.verticalLayout_5.addLayout(self.horizontalLayout_6)
+        self.verticalLayout_6.addWidget(self.groupBox)
+        self.horizontalLayout_5 = QtGui.QHBoxLayout()
+        self.horizontalLayout_5.setObjectName("horizontalLayout_5")
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.min_val_label = QtGui.QLabel(machine)
+        self.min_val_label.setStyleSheet("QLabel:disabled {\n"
+"    color: rgb(117, 117, 177);\n"
+"}\n"
+"QLabel:enabled {\n"
+"    color: rgb(255, 255, 255);\n"
+"}")
+        self.min_val_label.setObjectName("min_val_label")
+        self.verticalLayout.addWidget(self.min_val_label)
+        self.min_val = QtGui.QLineEdit(machine)
+        self.min_val.setEnabled(True)
+        self.min_val.setStyleSheet("QLineEdit:disabled {\n"
+"    color:rgb(117, 117, 117);\n"
+"    border: rgb(100, 100, 100);\n"
+"}\n"
+"QLineEdit:enabled {\n"
+"    color: rgb(255, 255, 255);\n"
+"    border: rgb(220, 220, 220);\n"
+"}")
+        self.min_val.setInputMethodHints(QtCore.Qt.ImhFormattedNumbersOnly)
+        self.min_val.setObjectName("min_val")
+        self.verticalLayout.addWidget(self.min_val)
+        self.horizontalLayout_5.addLayout(self.verticalLayout)
+        self.verticalLayout_2 = QtGui.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.max_val_label = QtGui.QLabel(machine)
+        self.max_val_label.setStyleSheet("QLabel:disabled {\n"
+"    color: rgb(117, 117, 177);\n"
+"}\n"
+"QLabel:enabled {\n"
+"    color: rgb(255, 255, 255);\n"
+"}")
+        self.max_val_label.setObjectName("max_val_label")
+        self.verticalLayout_2.addWidget(self.max_val_label)
+        self.max_val = QtGui.QLineEdit(machine)
+        self.max_val.setStyleSheet("QLineEdit:disabled {\n"
+"    color:rgb(117, 117, 117);\n"
+"    border: rgb(100, 100, 100);\n"
+"}\n"
+"QLineEdit:enabled {\n"
+"    color: rgb(255, 255, 255);\n"
+"    border: rgb(220, 220, 220);\n"
+"}")
+        self.max_val.setInputMethodHints(QtCore.Qt.ImhFormattedNumbersOnly)
+        self.max_val.setObjectName("max_val")
+        self.verticalLayout_2.addWidget(self.max_val)
+        self.horizontalLayout_5.addLayout(self.verticalLayout_2)
+        self.verticalLayout_3 = QtGui.QVBoxLayout()
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.default_val_label = QtGui.QLabel(machine)
+        self.default_val_label.setStyleSheet("QLabel:disabled {\n"
+"    color: rgb(117, 117, 177);\n"
+"}\n"
+"QLabel:enabled {\n"
+"    color: rgb(255, 255, 255);\n"
+"}")
+        self.default_val_label.setObjectName("default_val_label")
+        self.verticalLayout_3.addWidget(self.default_val_label)
+        self.default_val = QtGui.QLineEdit(machine)
+        self.default_val.setStyleSheet("QLineEdit:disabled {\n"
+"    color:rgb(117, 117, 117);\n"
+"    border: rgb(100, 100, 100);\n"
+"}\n"
+"QLineEdit:enabled {\n"
+"    color: rgb(255, 255, 255);\n"
+"    border: rgb(220, 220, 220);\n"
+"}")
+        self.default_val.setInputMethodHints(QtCore.Qt.ImhFormattedNumbersOnly)
+        self.default_val.setObjectName("default_val")
+        self.verticalLayout_3.addWidget(self.default_val)
+        self.horizontalLayout_5.addLayout(self.verticalLayout_3)
+        self.verticalLayout_6.addLayout(self.horizontalLayout_5)
+        self.verticalLayout_4 = QtGui.QVBoxLayout()
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+        self.enum_label = QtGui.QLabel(machine)
+        self.enum_label.setStyleSheet("QLabel:disabled {\n"
+"    color: rgb(117, 117, 177);\n"
+"}\n"
+"QLabel:enabled {\n"
+"    color: rgb(255, 255, 255);\n"
+"}")
+        self.enum_label.setObjectName("enum_label")
+        self.verticalLayout_4.addWidget(self.enum_label)
+        self.enum_list = QtGui.QLineEdit(machine)
+        self.enum_list.setStyleSheet("QLineEdit:disabled {\n"
+"    color:rgb(117, 117, 117);\n"
+"    border: rgb(100, 100, 100);\n"
+"}\n"
+"QLineEdit:enabled {\n"
+"    color: rgb(255, 255, 255);\n"
+"    border: rgb(220, 220, 220);\n"
+"}")
+        self.enum_list.setObjectName("enum_list")
+        self.verticalLayout_4.addWidget(self.enum_list)
+        self.verticalLayout_6.addLayout(self.verticalLayout_4)
+        self.horizontalLayout_4 = QtGui.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        self.out_attr_label = QtGui.QLabel(machine)
+        self.out_attr_label.setObjectName("out_attr_label")
+        self.horizontalLayout_4.addWidget(self.out_attr_label)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout_4.addItem(spacerItem)
+        self.in_attr_label = QtGui.QLabel(machine)
+        self.in_attr_label.setObjectName("in_attr_label")
+        self.horizontalLayout_4.addWidget(self.in_attr_label)
+        self.verticalLayout_6.addLayout(self.horizontalLayout_4)
         self.horizontalLayout_2 = QtGui.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.out_attr = QtGui.QGroupBox(machine)
-        self.out_attr.setMinimumSize(QtCore.QSize(0, 250))
-        self.out_attr.setObjectName("out_attr")
-        self.verticalLayoutWidget = QtGui.QWidget(self.out_attr)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 20, 311, 221))
-        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
-        self.out_attr_list = QtGui.QVBoxLayout(self.verticalLayoutWidget)
-        self.out_attr_list.setContentsMargins(0, 0, 0, 0)
-        self.out_attr_list.setObjectName("out_attr_list")
-        self.horizontalLayout_2.addWidget(self.out_attr)
+        self.out_attrs = QtGui.QComboBox(machine)
+        self.out_attrs.setObjectName("out_attrs")
+        self.horizontalLayout_2.addWidget(self.out_attrs)
         self.ConnectionLine = QtGui.QFrame(machine)
         self.ConnectionLine.setMaximumSize(QtCore.QSize(150, 16777215))
         self.ConnectionLine.setFrameShadow(QtGui.QFrame.Raised)
         self.ConnectionLine.setLineWidth(4)
         self.ConnectionLine.setObjectName("ConnectionLine")
         self.horizontalLayout_2.addWidget(self.ConnectionLine)
-        self.in_attr = QtGui.QGroupBox(machine)
-        self.in_attr.setObjectName("in_attr")
-        self.verticalLayoutWidget_2 = QtGui.QWidget(self.in_attr)
-        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(10, 20, 311, 221))
-        self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
-        self.in_attr_list = QtGui.QVBoxLayout(self.verticalLayoutWidget_2)
-        self.in_attr_list.setContentsMargins(0, 0, 0, 0)
-        self.in_attr_list.setObjectName("in_attr_list")
-        self.horizontalLayout_2.addWidget(self.in_attr)
-        self.verticalLayout_3.addLayout(self.horizontalLayout_2)
+        self.in_attrs = QtGui.QComboBox(machine)
+        self.in_attrs.setObjectName("in_attrs")
+        self.horizontalLayout_2.addWidget(self.in_attrs)
+        self.verticalLayout_6.addLayout(self.horizontalLayout_2)
+        self.value_label = QtGui.QLabel(machine)
+        self.value_label.setObjectName("value_label")
+        self.verticalLayout_6.addWidget(self.value_label)
         self.values = QtGui.QPlainTextEdit(machine)
         self.values.setFrameShape(QtGui.QFrame.Box)
         self.values.setLineWidth(2)
         self.values.setDocumentTitle("")
         self.values.setObjectName("values")
-        self.verticalLayout_3.addWidget(self.values)
+        self.verticalLayout_6.addWidget(self.values)
         self.horizontalLayout_3 = QtGui.QHBoxLayout()
         self.horizontalLayout_3.setObjectName("horizontalLayout_3")
-        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout_3.addItem(spacerItem)
-        self.do_it = QtGui.QPushButton(machine)
-        self.do_it.setObjectName("do_it")
-        self.horizontalLayout_3.addWidget(self.do_it)
+        spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem1)
+        self.reset_btn = QtGui.QPushButton(machine)
+        self.reset_btn.setObjectName("reset_btn")
+        self.horizontalLayout_3.addWidget(self.reset_btn)
         self.dont_do_it = QtGui.QPushButton(machine)
         self.dont_do_it.setObjectName("dont_do_it")
         self.horizontalLayout_3.addWidget(self.dont_do_it)
-        self.verticalLayout_3.addLayout(self.horizontalLayout_3)
+        self.do_it = QtGui.QPushButton(machine)
+        self.do_it.setObjectName("do_it")
+        self.horizontalLayout_3.addWidget(self.do_it)
+        self.verticalLayout_6.addLayout(self.horizontalLayout_3)
 
         self.retranslateUi(machine)
         QtCore.QMetaObject.connectSlotsByName(machine)
 
     def retranslateUi(self, machine):
         machine.setWindowTitle(QtGui.QApplication.translate("machine", "Attribute Machine", None))
-        self.label.setText(QtGui.QApplication.translate("machine", "Attribute Machine", None))
+        self.Title.setText(QtGui.QApplication.translate("machine", "Attribute Machine", None))
         self.modes_group.setTitle(QtGui.QApplication.translate("machine", "Modes", None))
         self.add_mode.setText(QtGui.QApplication.translate("machine", "Add", None))
         self.set_mode.setText(QtGui.QApplication.translate("machine", "Set", None))
         self.connect_mode.setText(QtGui.QApplication.translate("machine", "Connect", None))
         self.command_mode.setText(QtGui.QApplication.translate("machine", "Command", None))
-        self.out_attr.setTitle(QtGui.QApplication.translate("machine", "Out Attribute", None))
-        self.in_attr.setTitle(QtGui.QApplication.translate("machine", "In Attributes", None))
-        self.do_it.setText(QtGui.QApplication.translate("machine", "Do It", None))
+        self.groupBox.setTitle(QtGui.QApplication.translate("machine", "Attribute Type", None))
+        self.vector_btn.setText(QtGui.QApplication.translate("machine", "Vector", None))
+        self.int_btn.setText(QtGui.QApplication.translate("machine", "Integer", None))
+        self.string_btn.setText(QtGui.QApplication.translate("machine", "String", None))
+        self.float_btn.setText(QtGui.QApplication.translate("machine", "Float", None))
+        self.bool_btn.setText(QtGui.QApplication.translate("machine", "Boolean", None))
+        self.enum_btn.setText(QtGui.QApplication.translate("machine", "Enum", None))
+        self.min_val_label.setText(QtGui.QApplication.translate("machine", "Min", None))
+        self.min_val.setText(QtGui.QApplication.translate("machine", "0", None))
+        self.max_val_label.setText(QtGui.QApplication.translate("machine", "Max", None))
+        self.max_val.setText(QtGui.QApplication.translate("machine", "1", None))
+        self.default_val_label.setText(QtGui.QApplication.translate("machine", "Default", None))
+        self.default_val.setText(QtGui.QApplication.translate("machine", "0", None))
+        self.enum_label.setText(QtGui.QApplication.translate("machine", "Enum List (Comma Delineated)", None))
+        self.enum_list.setText(QtGui.QApplication.translate("machine", "Hi,Low", None))
+        self.out_attr_label.setText(QtGui.QApplication.translate("machine", "Out Attribute List", None))
+        self.in_attr_label.setText(QtGui.QApplication.translate("machine", "In Attribute List", None))
+        self.value_label.setText(QtGui.QApplication.translate("machine", "Values To Apply", None))
+        self.reset_btn.setText(QtGui.QApplication.translate("machine", "Reset Selection", None))
         self.dont_do_it.setText(QtGui.QApplication.translate("machine", "Don\'t Do It", None))
+        self.do_it.setText(QtGui.QApplication.translate("machine", "Do It", None))
 
