7de4f9273cce20c5d9f01c76611218b89d88c46a
Adam Benson <adambenson.vfx@gmail.com>
2019-11-12 11:34:53 -0800

    Logging system working

--- Python/Utilities/time_lord/tl_alpha_payroll_collector.py
+++ Python/Utilities/time_lord/tl_alpha_payroll_collector.py
@@ -15,10 +15,38 @@ from bin.time_continuum import continuum
 from datetime import datetime, timedelta
 from dateutil import parser
 import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
+import sys
 
 
 __author__ = 'Adam Benson'
 __version__ = '0.0.1'
 
 config = configuration.get_configuration()
-print config
+
+# ------------------------------------------------------------------------------------------------------
+# Create logging system
+# ------------------------------------------------------------------------------------------------------
+log_file = 'tl_alpha_payroll_collect.log'
+log_root = os.path.join(sys.path[0], 'logs')
+if not os.path.exists(log_root):
+    os.makedirs(log_root)
+log_path = os.path.join(log_root, log_file)
+debug = config['debug_logging']
+if debug == 'True' or debug == 'true' or debug == True:
+    level = logging.DEBUG
+else:
+    level = logging.INFO
+logger = logging.getLogger('payroll_collect')
+logger.setLevel(level=level)
+fh = TimedRotatingFileHandler(log_path, when='%s' % config['log_interval'], interval=1,
+                              backupCount=int(config['log_days']))
+fm = logging.Formatter(fmt='%(asctime)s - %(name)s | %(levelname)s : %(lineno)d - %(message)s')
+fh.setFormatter(fm)
+logger.addHandler(fh)
+
+logger.info('Alpha Payroll Collection Utility has started.')
+
+
+
