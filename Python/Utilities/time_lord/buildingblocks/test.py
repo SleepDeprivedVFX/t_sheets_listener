0e77e1da1dc8cc1f4528d86d8ac68215dde3f0ea
Adam Benson <adambenson.vfx@gmail.com>
2019-12-16 15:21:51 -0800

    Tested a windows session lock script.  It only works when the password window is up.  Otherwise it just assumes it is unlocked.  Doesn't really work.

--- Python/Utilities/time_lord/buildingblocks/test.py
+++ Python/Utilities/time_lord/buildingblocks/test.py
@@ -16,15 +16,43 @@
 # print response
 #
 
-intersect = set(range(2000)) & set(range(1000, 3000))
-print intersect
+# intersect = set(range(2000)) & set(range(1000, 3000))
+# print intersect
+#
+# a = 10
+# b = 4
+#
+# print(a & b)
+# print(a | b)
+# print(~a)
+# print(range(2000))
+# print(set(range(2000)))
+# print range(1000, 3000)
 
-a = 10
-b = 4
 
-print(a & b)
-print(a | b)
-print(~a)
-print(range(2000))
-print(set(range(2000)))
-print range(1000, 3000)
+import time
+import ctypes
+
+user32 = ctypes.windll.User32
+OpenDesktop = user32.OpenDesktopA
+SwitchDesktop = user32.SwitchDesktop
+DESKTOP_SWITCHDESKTOP = 0x0100
+
+# user32.LockWorkStation()
+#
+# Slight pause to overcome what appears to
+# be a grace period during which a switch
+# *will* succeed.
+#
+time.sleep(1.0)
+
+while 1:
+    hDesktop = OpenDesktop("default", 0, False, DESKTOP_SWITCHDESKTOP)
+    result = SwitchDesktop(hDesktop)
+    if result:
+        print "Unlocked", time.asctime()
+        time.sleep(2)
+        # break
+    else:
+        print time.asctime(), "still locked"
+        time.sleep(2)
