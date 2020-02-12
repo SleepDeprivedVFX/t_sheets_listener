709d615ccd44ccd7f6f570ff76795af9b84b325a
Adam Benson <AdamBenson.vfx@gmail.com>
2020-02-11 19:49:35 -0800

    Properly exports a project based reports system

--- Python/Utilities/time_lord/tl_reports.py
+++ Python/Utilities/time_lord/tl_reports.py
@@ -437,6 +437,9 @@ class reports_ui(QtGui.QWidget):
         # Connect report processors
         self.engine.signals.snd_report_project_hours.connect(self.project_hours_report)
 
+        # Create Main EXCEL sheet
+        self.report = xls.Workbook('temp.xlsx')
+
     def run_reports(self):
         primary = self.ui.primary_org.currentText()
         primary_id = self.ui.primary_org.itemData(self.ui.primary_org.currentIndex())
@@ -476,7 +479,6 @@ class reports_ui(QtGui.QWidget):
         self.ui.end_time.setDate(guess_end_date)
 
     def project_hours_report(self, data=None):
-        print('DATA RECIEVED: %s' % data.keys())
         if data:
             self.ui.data_tree.clear()
             # header = self.ui.data_tree.horizontalHeader()
@@ -493,36 +495,58 @@ class reports_ui(QtGui.QWidget):
             shot_steps = data['shot_steps']
             tree_structure = data['tree_structure']
 
+            report_page = self.report.add_worksheet('Project Actuals')
+            report_page.set_column('A:A', 20)
+            bold = self.report.add_format({'bold': True})
+            highlight = self.report.add_format({'bg_color': 'yellow'})
+            heading = self.report.add_format({'bg_color': '#CCFFFF'})
+
+            report_page.write(0, 0, 'Actuals Report for Projects')
+            report_page.write('A3', 'Project', heading)
+            report_page.write('B3', 'Entity Type', heading)
+            report_page.write('C3', 'Entity', heading)
+            report_page.write('D3', 'Task', heading)
+            report_page.write('E3', 'Artist', heading)
+            report_page.write('F3', 'Proj Hrs', heading)
+            report_page.write('G3', 'Type Hrs', heading)
+            report_page.write('H3', 'Type %% Total', heading)
+            report_page.write('I3', 'Entity Hrs', heading)
+            report_page.write('J3', 'Entity %% Total', heading)
+            report_page.write('K3', 'Task Hrs', heading)
+            report_page.write('L3', 'Task %% Total', heading)
+            report_page.write('M3', 'Artist Hrs', heading)
+            report_page.write('N3', 'Artist %% Total', heading)
+
+            row = 3
+
             # Build the tree
-            proj_count = len(tree_structure.keys())
             for proj, details in tree_structure.items():
                 proj_label = QtGui.QTreeWidgetItem()
                 proj_label.setText(0, proj)
                 proj_duration = float(details['_duration_']) / 60.0
-                proj_label.setText(6, 'A: %0.2f hrs' % proj_duration)
+                proj_label.setText(5, '%0.2f hrs' % proj_duration)
 
-                ent_type_count = len(details.keys())
-                # Entity typ will be Asset or Shot
+                # Excel
+                row += 1
+                report_page.write(row, 0, proj)
+                report_page.write(row, 5, '%0.2f hrs' % proj_duration)
+
                 for ent_type, entities in details.items():
-                    # Get the current averages.
-                    avg = 0.0
-                    for k, v in details['_avgs_'].items():
-                        if k == ent_type:
-                            avg += v
-                    avg = (avg / proj_count) / 60.0
 
                     if ent_type not in ['_duration_', '_avg_time_', '_avgs_']:
                         ent_type_label = QtGui.QTreeWidgetItem()
                         ent_type_label.setText(1, ent_type)
                         ent_type_duration = float(entities['_duration_']) / 60.0
-                        ent_type_label.setText(6, 'B: %0.2f hrs' % ent_type_duration)
-                        # FIXME: This average is wrong.  And I may need to just get rid of the _avg_time_ field
-                        # ent_type_avg = (sum(details['_avg_time_']) / float(ent_type_count)) / 60.0
+                        ent_type_label.setText(5, '%0.2f hrs' % ent_type_duration)
                         ent_type_avg = ((ent_type_duration / proj_duration) * 100)
-                        ent_type_label.setText(7, 'C: %0.2f %%' % ent_type_avg)
-                        # ent_type_label.setText(7, '%s avg: %0.2f hrs' % (ent_type, avg))
+                        ent_type_label.setText(6, '%0.2f%% time' % ent_type_avg)
 
-                        entity_count = len(entities.keys())
+                        # Excel
+                        row += 1
+                        report_page.write(row, 1, ent_type)
+                        report_page.write(row, 6, '%0.2f hrs' % ent_type_duration)
+                        report_page.write(row, 7, '%0.2f%%' % ent_type_avg)
+
                         for entity, steps in entities.items():
                             # Get the current averages.
                             avg = 0.0
@@ -534,58 +558,55 @@ class reports_ui(QtGui.QWidget):
                                 entity_label = QtGui.QTreeWidgetItem()
                                 entity_label.setText(2, entity)
                                 entity_duration = float(steps['_duration_']) / 60.0
-                                entity_label.setText(6, 'D: %0.2f Hrs' % entity_duration)
-                                # FIXME: I think this average is way wrong too
+                                entity_label.setText(5, '%0.2f Hrs' % entity_duration)
                                 entity_avg = (float(entity_duration) / float(ent_type_duration)) * 100.0
-                                entity_label.setText(7, 'E: %0.2f %%' % entity_avg)
-                                step_count = len(steps)
+                                entity_label.setText(6, '%0.2f%% time' % entity_avg)
+
+                                # Excel
+                                row += 1
+                                report_page.write(row, 2, entity)
+                                report_page.write(row, 8, '%0.2f hrs' % entity_duration)
+                                report_page.write(row, 9, '%0.2f%%' % entity_avg)
+
                                 for step, tasks in steps.items():
                                     if step not in ['_duration_', '_avg_time_', '_avgs_']:
                                         step_label = QtGui.QTreeWidgetItem()
                                         step_label.setText(3, step)
                                         step_duration = float(tasks['_duration_']) / 60.0
-                                        step_label.setText(6, 'F: %0.2f hrs' % step_duration)
-                                        # FIXME: As well as this one...
-                                        # step_average = (sum(tasks['_avg_time_']) / float(step_count)) / 60.0
+                                        step_label.setText(5, '%0.2f hrs' % step_duration)
                                         step_average = step_duration / entity_duration * 100.0
-                                        step_label.setText(7, 'G: %0.2f %%' % step_average)
-                                        print('task_items: %s' % tasks.items())
-                                        # for artist, info in tasks.items():
-                                        #     print('ARTIST: %s' % artist)
-                                        #     if artist not in ['_duration_', '_avg_time_', '_avgs_', 'timesheets']:
-                                        #         print('INFO: %s' % info)
-                                        #         # artist = tasks['timesheets']['user']['name']
-                                        #         artist_label = QtGui.QTreeWidgetItem()
-                                        #         artist_label.setText(4, artist)
-                                        #         artist_label.setText(6, 'Total: %0.2f hrs' % step_duration)
-                                        #         artist_avg_count = len(tasks['_avg_time_'])
-                                        #         artist_avg = (sum(tasks['_avg_time_']) / float(artist_avg_count)) / 60.0
-                                        #         artist_label.setText(7, 'Avg Per: %0.2f hrs' % artist_avg)
-                                        #
-                                        #         # print('TASK DUMP: %s' % tasks)
-                                        #         step_label.addChild(artist_label)
-                                        # timesheet_list = tasks['timesheets']
-                                        # for sheet in timesheet_list:
-                                        #     artist = sheet['user']['name']
-                                        #     artist_
-                                        #     artist_label = QtGui.QTreeWidgetItem()
-                                        #     artist_label.setText(4)
+                                        step_label.setText(6, '%0.2f%% time' % step_average)
                                         step_artists = tasks['_avgs_']
+
+                                        # Excel
+                                        row += 1
+                                        report_page.write(row, 3, step)
+                                        report_page.write(row, 10, '%0.2f hrs' % step_duration)
+                                        report_page.write(row, 11, '%0.2f%%' % step_average)
+
                                         for artist, hours in step_artists.items():
                                             artist_label = QtGui.QTreeWidgetItem()
                                             artist_label.setText(4, artist)
-                                            artist_label.setText(6, 'H: %0.2f Hrs' % (hours / 60.0))
+                                            artist_label.setText(5, '%0.2f Hrs' % (hours / 60.0))
                                             artist_avg = (hours / 60.0) / step_duration * 100.0
-                                            artist_label.setText(7, 'I: %0.2f %%' % artist_avg)
+                                            artist_label.setText(6, '%0.2f%% time' % artist_avg)
                                             step_label.addChild(artist_label)
 
+                                            # Excel
+                                            row += 1
+                                            report_page.write(row, 4, artist)
+                                            report_page.write(row, 12, '%0.2f hrs' % (hours / 60.0))
+                                            report_page.write(row, 13, '%0.2f%%' % artist_avg)
 
                                         entity_label.addChild(step_label)
                                 ent_type_label.addChild(entity_label)
                         proj_label.addChild(ent_type_label)
 
                 self.ui.data_tree.addTopLevelItem(proj_label)
-            self.ui.data_tree.resizeColumnToContents(6)
+            # self.ui.data_tree.resizeColumnToContents(5)
+            # self.ui.data_tree.resizeColumnToContents(6)
+            # self.ui.data_tree.resizeColumnToContents(7)
+            self.report.close()
 
     def set_search_options(self, driver=None, list=None):
         drv_obj = driver.currentText()
