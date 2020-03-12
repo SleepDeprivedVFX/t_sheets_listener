5eda971b4fdf529868f7d5eb8b83915b3ef9bc2d
Adam Benson <adambenson.vfx@gmail.com>
2020-03-11 18:17:47 -0700

    Laying out the most basic of UI setups

--- Python/Utilities/time_lord/time_lord_dev_4.py
+++ Python/Utilities/time_lord/time_lord_dev_4.py
@@ -79,3 +79,126 @@ lunch_task = sg_data.get_lunch_task(lunch_proj_id=int(config['admin_proj_id']),
 comm = comm_system.comm_sys(sg, config=config, sub='time_lord')
 logger.info('Communication system online.')
 
+# -------------------------------------------------------------------------------------------------------------------
+# Stream Handler
+# -------------------------------------------------------------------------------------------------------------------
+# NOTE: Do I actually need the stream handler?  Can I get by (or be better off) with more practical messages being
+#       posted to the UI?
+
+
+# ------------------------------------------------------------------------------------------------------
+# Signal Emitters
+# ------------------------------------------------------------------------------------------------------
+# NOTE: Might be best to create these as needed, rather than copying the existing ones.
+class time_signals(QtCore.QObject):
+    # Logger Signals
+
+    # Timesheet Signals
+    get_timesheet = QtCore.Signal(dict)
+    set_timesheet = QtCore.Signal(dict)
+
+# -------------------------------------------------------------------------------------------------------------------
+# Clocks Engine
+# -------------------------------------------------------------------------------------------------------------------
+# NOTE: The clock engines will continue to run the UI clocks.
+
+# ------------------------------------------------------------------------------------------------------
+# Primary Engine
+# ------------------------------------------------------------------------------------------------------
+# NOTE: time_machine will continue to run services, an/or be the outside event listener.
+#       Or, it will be integrated into the clock? No. Probably not. I want time features to continue
+
+# ------------------------------------------------------------------------------------------------------
+# Primary Tools
+# ------------------------------------------------------------------------------------------------------
+# NOTE: time_lord currently does most of the processing
+
+
+# ------------------------------------------------------------------------------------------------------
+# User Interface
+# ------------------------------------------------------------------------------------------------------
+# NOTE: time_lord_ui needs to only be display and buttons. NO PROCESSES!!!
+class time_lord_ui(QtWidgets.QMainWindow):
+    """
+    The main Time Lord UI.
+    """
+    def __init__(self, parent=None):
+        QtWidgets.QMainWindow.__init__(self, parent)
+
+        # --------------------------------------------------------------------------------------------------------
+        # Set the saved settings
+        # --------------------------------------------------------------------------------------------------------
+        # This saves all the last settings so that they return to their previous state when the program is run again.
+        self.settings = QtCore.QSettings(__author__, 'TimeLord')
+        self.saved_project = self.settings.value('project', '.')
+        self.saved_project_id = self.settings.value('project_id', '.')
+        self.saved_entity = self.settings.value('entity', '.')
+        self.saved_entity_id = self.settings.value('entity_id', '.')
+        self.saved_task = self.settings.value('task', '.')
+        self.saved_task_id = self.settings.value('task_id', '.')
+        self.saved_window_position = self.settings.value('geometry', '')
+        self.restoreGeometry(self.saved_window_position)
+
+        # --------------------------------------------------------------------------------------------------------
+        # Setup Time Engine
+        # --------------------------------------------------------------------------------------------------------
+        # Setup and connect the last timesheet.
+        # Declare Class Variables
+        self.latest_timesheet = tl_time.get_latest_timesheet(user=user)
+
+        # FIXME: The following is all processes.  Should not be handled here.
+        # If the user has no previous timesheet, create an empty one here.
+        if not self.latest_timesheet['project'] and not self.latest_timesheet['entity'] \
+                and not self.latest_timesheet['date'] and not self.latest_timesheet['sg_task_start']:
+            project_id = int(config['admin_proj_id'])
+            task_id = int(config['admin_task_id'])
+            entity_id = None
+            context = {
+                'Project': {
+                    'id': project_id
+                },
+                'Task': {
+                    'id': task_id
+                },
+                'Entity': {
+                    'id': entity_id,
+                }
+            }
+            start_time = datetime.now()
+            first_sheet = tl_time.create_new_timesheet(user=user, context=context, start_time=start_time)
+            if first_sheet:
+                tl_time.clock_out_time_sheet(timesheet=first_sheet, clock_out=start_time)
+
+        self.clocked_in = tl_time.is_user_clocked_in(user=user)
+
+        # --------------------------------------------------------------------------------------------------------
+        # Setup UI
+        # --------------------------------------------------------------------------------------------------------
+        self.ui = tlu.Ui_TimeLord()
+        self.ui.setupUi(self)
+        self.setWindowIcon(QtGui.QIcon('icons/tl_icon.ico'))
+        self.setWindowTitle("Time Lord v%s" % __version__)
+        self.window_on_top_tested = False
+        # self.set_window_on_top()
+
+        # Set main user info
+        self.ui.artist_label.setText(user['name'])
+
+
+if __name__ == '__main__':
+    os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+    app = QtWidgets.QApplication(sys.argv)
+    app.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+    # app.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+    app.setOrganizationName('AdamBenson')
+    app.setOrganizationDomain('adamdbenson.com')
+    app.setApplicationName('TimeLord')
+    splash_pix = QtGui.QPixmap('ui/resources/Time_Lord_Logo.png')
+    splash = QtWidgets.QSplashScreen(splash_pix, QtCore.Qt.WindowStaysOnTopHint)
+    splash.setMask(splash_pix.mask())
+    splash.show()
+    app.processEvents()
+    window = time_lord_ui()
+    window.show()
+    splash.finish(window)
+    sys.exit(app.exec_())
